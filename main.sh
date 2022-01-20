2c5c000d7c3c773229b2b1cbf86cb5121563b74e
Edge <69719569+omxpro@users.noreply.github.com>
2022-01-20 21:43:33 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -13,5 +13,6 @@ echo "${cyan} https://discord.gg/notsaksh${reset}"
 echo "${yellow} =======================================================${reset}" && sleep 10s
 echo "${cyan} ===========================================${reset}"
 echo "${green}i assume you have installed packages so i am starting the bots, remember not to add too many bots as your server can get overloaded if you add more, also this is about to be spammed with alot of bor starting messages, dont be scared, also you have to fill out start.js by yourself its not that complicated and if you need any help please join our server and contact for help, see the index.js file to see the format, and hey also check your config.json or config.yml or .env file as we've only filled out prefix and token field, make sure to fill out other fields" && sleep 15s
-node start.js
+wget https://raw.githubusercontent.com/omxpro/bot-maker-files/main/start.js && sleep 2s
+node start.js 
 echo "${cyan} ===========================================${reset}"
